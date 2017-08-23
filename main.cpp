7ce96f5c820af96ee18130fc3d1db5a34595a044
avberrio <avberrio@github.com>
2017-08-23 17:10:38 -0300

    First commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ppa_project
+//
+//  Created by Aldo Berrios Valenzuela on 23-8-17.
+//  Copyright © 2017 Aldo Berrios Valenzuela. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
